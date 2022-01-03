866448b82de3e013f555e5c79aeb7e12b5951d39
Pragya <pragyakhushirai@gmail.com>
2022-01-03 14:53:12 +0530

    github Action Take 5

--- cypress/integration/MessageApp.spec.js
+++ cypress/integration/MessageApp.spec.js
@@ -2,6 +2,7 @@ describe('Test Message app',()=>{
     it('Test Message App',()=>{
         cy.visit('http://localhost:3000/')
         cy.get("h1[class='display-4']").should('have.text', ' Send Message')
+        console.log("Pragya")
         
 
     })
